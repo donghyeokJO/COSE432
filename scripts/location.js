78c63191c43ee3761a87b331512e629ef6b3ab1a
donghyeokJO <49118254+donghyeokJO@users.noreply.github.com>
2021-12-07 02:41:20 +0900

    address concat

--- scripts/location.js
+++ scripts/location.js
@@ -1,10 +1,11 @@
 function getCityGU() {
     new daum.Postcode({
         oncomplete: function(data) {
-            var addr = data.sigungu; // 최종 주소 변수
+            var si = data.sido
+            var sigungu = data.sigungu; // 최종 주소 변수
 
             // 주소 정보를 해당 필드에 넣는다.
-            document.getElementById("location-text").value = addr;
+            document.getElementById("location-text").value = si + ' ' + sigungu;
             // 주소로 상세 정보를 검색
         }
     }).open();
