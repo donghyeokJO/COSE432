82742df8d086d62001954fa354e5f4697942f6ad
donghyeokJO <49118254+donghyeokJO@users.noreply.github.com>
2021-12-08 01:37:42 +0900

    지도에서 위치선택

--- scripts/location.js
+++ scripts/location.js
@@ -3,8 +3,12 @@ function getCityGU() {
         oncomplete: function(data) {
             var si = data.sido
             var sigungu = data.sigungu; // 최종 주소 변수
-            console.log(si);
-            console.log(sigungu);
+            // console.log(si);
+            // console.log(sigungu);
+
+            if (si == "서울") {
+                si += '시'
+            }
             // 주소 정보를 해당 필드에 넣는다.
             document.getElementById("location-text").value = si + ' ' + sigungu;
         }
